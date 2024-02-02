745a63edaabce0d9c4cf602c70cb45319ebba7ab
MiriamBabukhian <108429802+MiriamBabukhian@users.noreply.github.com>
2024-02-02 01:51:16 +0100

    Add files via upload

---
+++ DNABert2_training/main_DNABert2.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 00:56:42 2024
+
+@author: miriambabukhian
+"""
+from utils_DNABert2 import FivePrimeSeqs, train, validation, plot_ism_predictions, predict_wild_type, calculate_gc_percentage
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import roc_curve, auc
+import torch 
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoConfig, AdamW, get_linear_schedule_with_warmup, set_seed
+import seaborn as sns
+import scipy
+
+data = pd.read_csv("traindata_cnn.csv") 
+
+#exclude sequences for mutagenesis
+brain_utrs_ids = ["ENST00000404241", "ENST00000463675", "ENST00000084798", "ENST00000409851", "ENST00000320717",
+                  "ENST00000372677", "ENST00000355994", "ENST00000303177", "ENST00000425191", "ENST00000326695"]
+mask = pd.notna(data['ensembl_transcript_id']) & data['ensembl_transcript_id'].str.contains('|'.join(brain_utrs_ids), na=False)
+brainutrs_df = data[mask]
+print(brainutrs_df)
+excluded_data = data[~mask]
+data = excluded_data
+
+#create feats and labels arrays
+Feats = np.array(data["CUTseq"])[:, np.newaxis]
+ClassLabels = np.array(data["label"])[:, np.newaxis]
+labels_ids = {'random': 0, '5UTR':1}
+
+#exclude sequences for gc-pred correlation, the sequences are randomly picked
+gc_seqs = Feats[300:340].flatten().tolist()
+gc_labels = ClassLabels[300:340]
+feats_excluded = np.concatenate((Feats[:300], Feats[340:]))
+Feats = feats_excluded
+labels_excluded = np.concatenate((ClassLabels[:300], ClassLabels[340:]))
+ClassLabels = labels_excluded
+
+# define parameters
+set_seed(123)
+batch_size = 32
+epochs = 3
+
+#select device 
+#for MacBook M1 and M2 run: 
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu') 
+print('Using device:', device)  
+
+#For CUDA devices: 
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#print('Using device:', device)
+
+# define model config
+n_labels = len(labels_ids)
+config = AutoConfig.from_pretrained("zhihan1996/DNABERT-2-117M", num_labels = n_labels, output_attentions = True)
+
+#load tokenizer
+tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M")
+
+#define model
+model = AutoModelForSequenceClassification.from_pretrained("zhihan1996/DNABERT-2-117M", config = config)
+model.to(device)
+print('Model loaded to `%s`'%device)
+
+#test train split
+X_train, X_test, y_train, y_test = train_test_split(Feats, ClassLabels,
+                                                    test_size=0.2, random_state=42, stratify= ClassLabels)
+
+
+# load train data,  create Pytorch dataset
+train_dataset = FivePrimeSeqs(X_train.flatten().tolist(), tokenizer, y_train)
+
+# move data into DataLoader (send training data in batches)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+print('Created `train_dataloader` with %d batches!'%len(train_dataloader))
+
+
+# validation step (same as training but with test data)
+
+test_dataset = FivePrimeSeqs(X_test.flatten().tolist(), tokenizer, y_test)
+
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+print('Created `test_dataloader` with %d batches!'%len(test_dataloader))
+
+
+
+# define optimizer
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5, # args.learning_rate - default is 5e-5,
+                  eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
+                  )
+
+# Total number of training steps is number of batches * number of epochs.
+# `train_dataloader` contains batched data so `len(train_dataloader)` gives the number of batches.
+total_steps = len(train_dataloader) * epochs
+
+# Create the learning rate scheduler.
+scheduler = get_linear_schedule_with_warmup(optimizer,
+                                            num_warmup_steps = 0, # Default value 
+                                            num_training_steps = total_steps)
+
+
+# send data to train and eval
+
+#store loss and accuracy for plotting
+all_loss = {'train_loss':[], 'val_loss':[]}
+all_acc = {'train_acc':[], 'val_acc':[]}
+
+# Loop through each epoch.
+print('Epoch')
+for epoch in tqdm(range(epochs)):
+  print()
+  print('Training on batches...')
+  # Perform one full pass over the training set.
+  train_labels, train_predict, train_loss = train(train_dataloader, model, optimizer, scheduler, device)
+  train_acc = accuracy_score(train_labels, train_predict)
+
+  # Get prediction form model on validation data.
+  print('Validation on batches...')
+  valid_labels, valid_predict, val_proba, val_loss, attention = validation(test_dataloader, model, device)
+  val_acc = accuracy_score(valid_labels, valid_predict)
+
+  # Print loss and accuracy values 
+  print("  train_loss: %.5f - val_loss: %.5f - train_acc: %.5f - valid_acc: %.5f"%(train_loss, val_loss, train_acc, val_acc))
+  print()
+
+  all_loss['train_loss'].append(train_loss)
+  all_loss['val_loss'].append(val_loss)
+  all_acc['train_acc'].append(train_acc)
+  all_acc['val_acc'].append(val_acc)
+  
+
+#plot accuracy curve over epochs
+plt.plot(range(1, len(all_acc['train_acc']) + 1), all_acc['train_acc'])
+plt.plot(range(1, len(all_acc['val_acc']) + 1), all_acc['val_acc'])
+plt.ticklabel_format(style='plain', axis='x', useOffset=False)
+plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.title('DNABert-2 accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+#plot loss curve over epochs
+plt.plot(range(1, len(all_loss['train_loss']) + 1), all_loss['train_loss'])
+plt.plot(range(1, len(all_loss['val_loss']) + 1), all_loss['val_loss'])
+plt.ticklabel_format(style='plain', axis='x', useOffset=False)
+plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.title('DNABert-2 loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+#store values for confusion matrix
+true_labels, predictions_labels, prediction_proba, avg_epoch_loss, attention = validation(test_dataloader, model, device)
+
+# Create the evaluation report.
+evaluation_report = classification_report(true_labels, predictions_labels, labels=list(labels_ids.values()), target_names=list(labels_ids.keys()))
+# Show the evaluation report.
+print(evaluation_report)
+
+#plot confusion matrix
+cm = confusion_matrix(true_labels, predictions_labels, normalize='pred')
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels= {'random': 0, '5UTR': 1})
+disp.plot(cmap='Blues')
+disp.ax_.set_title('Normalized Confusion Matrix-CNN')
+disp.ax_.grid(False)
+plt.show()
+    
+#select predicted probabilities for ROC-AUC
+preds = prediction_proba[:, 1:2] 
+fpr, tpr, thresholds = roc_curve(true_labels, preds)
+roc_auc = auc(fpr, tpr)
+
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr, tpr, label='DNABert (area = {:.3f})'.format(roc_auc))
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend(loc='best')
+
+#plt.savefig('cnn_roc.pdf', format='pdf')
+plt.show()
+
+
+#In Silico Mutagensis 
+mut_seqs = np.array(brainutrs_df["CUTseq"])[:, np.newaxis]
+mut_labels = np.array(brainutrs_df["label"])[:, np.newaxis]
+
+wd_encodings, wd_preds, wd_att, mut_preds = plot_ism_predictions(tokenizer, mut_seqs.flatten().tolist(), mut_labels, model, device)
+
+#plot Heatmap of the attention scores, visualization has to be done per sequence and per head.
+seq = 2
+attention_head = 3
+s = sns.heatmap(wd_att[seq, attention_head, :25, :25], cmap = "viridis", annot=False)
+s.set(title = f'Attention - Sequence {seq + 1}, Head {attention_head}')
+plt.show()
+    
+
+#tokenizer vocabulary 
+vocabulary = tokenizer.get_vocab()
+target_token_id = 188
+for i, (token, token_id) in enumerate(vocabulary.items()):
+    if token_id == target_token_id:
+        print(f"Found the target token '{token}' with ID {target_token_id} in the vocabulary.")
+        break  # Stop the loop if the target token is found
+
+
+#Calculate GC-content of previously selected sequences
+gc_content = []
+for seq in gc_seqs:
+  gc_content.append(calculate_gc_percentage(seq))
+  
+gc_encs, gc_preds, gc_att = predict_wild_type(tokenizer, gc_seqs, gc_labels, model, device)
+
+# Create a scatter plot
+plt.scatter(gc_content, gc_preds, color='blue', alpha=0.5)
+
+# Set axis labels and title
+plt.xlabel('GC Content')
+plt.ylabel('Predicted Probability')
+plt.title('GC Content vs. Predicted Probability- DNABert-2')
+
+# Show the plot
+plt.show()
+
+#calculate distance correlation
+gc_arr = np.array(gc_content)
+def calculate_dist_corr(gc_cont, predicted_proba):
+  dist_corr = scipy.spatial.distance.correlation(gc_cont,predicted_proba)
+  return dist_corr
+calculate_dist_corr(gc_arr,gc_preds.flatten())
+
+
+
+    
+    
+    
+    
+    
+
---
+++ DNABert2_training/utils_DNABert2.py
@@ -0,0 +1,429 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 01:19:16 2024
+
+@author: miriambabukhian
+"""
+
+from tqdm import tqdm
+import torch 
+from torch import nn 
+import random
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+
+
+class FivePrimeSeqs(Dataset):
+  r"""PyTorch Dataset class for loading data.
+    
+  Arguments:
+    inputs_ids (:obj:`str`):
+        input sequences.
+    
+    use_tokenizer (:obj:`transformers.tokenization_?`):
+        Transformer type tokenizer used to process raw sequences into numbers.
+        
+    labels_ids (:obj):
+        store sequences labels 
+ 
+  """
+  def __init__(self, input_ids, tokenizer, labels): 
+      
+      self.inputs = tokenizer.batch_encode_plus(input_ids,  add_special_tokens= True, 
+                                                max_length=512, truncation = False, 
+                                                padding = True, return_attention_mask=True, return_tensors = "pt")
+   # Add labels
+      self.n_examples = len(labels)
+      self.inputs.update({'labels':torch.tensor(labels)})
+      print('Finished!\n') 
+   
+      return 
+   
+  def __len__(self): 
+      
+      return self.n_examples
+     
+ 
+  def __getitem__(self, item): 
+      
+      return{key: self.inputs[key][item] for key in self.inputs.keys()} 
+  
+    
+  
+    
+  
+def train(dataloader, model, optimizer, scheduler, device): 
+  r"""
+  Train model on a single pass through the dataloader.
+
+  Arguments:
+
+      dataloader (:obj:`torch.utils.data.dataloader.DataLoader`):
+          Parsed data into batches of tensors.
+
+      optimizer_ (:obj:`transformers.optimization.AdamW`):
+          Optimizer used for training.
+
+      scheduler_ (:obj:`torch.optim.lr_scheduler.LambdaLR`):
+          PyTorch scheduler.
+
+      device_ (:obj:`torch.device`):
+          Device used to load data before model training.
+
+  Returns:
+
+      :obj:`List of [True Labels, Predicted
+        Labels, Average Loss].
+  """
+
+ # Tracking variables.
+  predictions_labels = []
+  true_labels = []
+ # Total loss for this epoch.
+  total_loss = 0
+
+  model.train()
+
+  for batch in tqdm(dataloader, total=len(dataloader)):
+
+   true_labels += batch['labels'].flatten().tolist()
+   
+   # move batch to device
+   batch = {k:v.type(torch.long).to(device) for k,v in batch.items()}
+   
+   model.zero_grad() 
+ 
+   outputs = model(**batch)
+   
+   loss, logits = outputs[:2] 
+   
+   total_loss += loss.item()
+
+   # Perform a backward pass to calculate the gradients.
+   loss.backward() 
+
+   # Clip the norm of the gradients to 1.0.
+   # This is to help prevent the "exploding gradients" problem.
+   torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) 
+
+   # Update parameters 
+  
+   optimizer.step() 
+
+   # Update the learning rate.
+  
+   scheduler.step() 
+
+   # Move logits and labels to CPU
+   logits = logits.detach().cpu().numpy()
+   
+    # Convert these logits to list of predicted labels values.
+   predictions_labels += logits.argmax(axis=-1).flatten().tolist()
+
+  avg_epoch_loss = total_loss / len(dataloader)
+  
+  # Return all true labels and prediction for future evaluations.
+  return true_labels, predictions_labels, avg_epoch_loss  
+  
+
+
+def validation(dataloader, model, device_):
+  r"""Validation function to evaluate model performance on a 
+  separate set of data.
+  
+  Arguments:
+    dataloader (:obj:`torch.utils.data.dataloader.DataLoader`):
+          Parsed data into batches of tensors.
+    device_ (:obj:`torch.device`):
+          Device used to load data before model training.
+          
+    model (:obj:'our model')
+  Returns:
+    
+    :obj:`List of [True Labels, Predicted
+        Labels, Predicted Probabilities, Average Loss, Attention Scores]
+  """
+  
+  # Tracking variables
+  predictions_labels = []
+  true_labels = []
+  probabilities = []
+ #total loss for this epoch.
+  total_loss = 0
+
+  model.eval()
+
+  for batch in tqdm(dataloader, total=len(dataloader)):
+
+   true_labels += batch['labels'].flatten().tolist()
+
+   # move batch to device
+   batch = {k:v.type(torch.long).to(device_) for k,v in batch.items()}
+
+   with torch.no_grad():  
+        
+        outputs = model(**batch) 
+        
+        #print attention weights for last layer
+        attentions = outputs.attentions[-1] 
+
+        loss, logits = outputs[:2] 
+        
+        # Move logits, labels and attention to CPU
+        logits = logits.detach().cpu().numpy() 
+        attentions = attentions.detach().cpu().numpy()
+ 
+        total_loss += loss.item() 
+        
+        #get prediction probabilities by passing the model's logits through a Softmax function
+        probabilities += nn.functional.softmax(torch.tensor(logits), dim=-1) 
+        prediction_proba = torch.stack(probabilities) 
+        prediction_proba = prediction_proba.numpy() 
+
+        predict_content = logits.argmax(axis=-1).flatten().tolist()
+
+        predictions_labels += predict_content 
+
+  # Calculate the average loss over the training data.
+  avg_epoch_loss = total_loss / len(dataloader)
+  
+  # Return all true labels and predictiond for future evaluations.
+  return true_labels, predictions_labels, prediction_proba, avg_epoch_loss, attentions 
+
+
+
+
+def generate_mutated_sequences(sequence):
+    r"""Generate mutated sequences for ISM
+    
+    Arguments:
+        sequence (:obj:`str`):
+            input sequence.
+    Returns:
+      
+      :obj:`List of mutated sequences
+    """
+    
+    sequence_alts = []
+    for index, ref in enumerate(sequence):
+        possible_nucleotides = ['A', 'C', 'G', 'T']
+        
+        #make sure that the function does NOT select the original nucleotide 
+        possible_nucleotides.remove(ref) if ref in possible_nucleotides else None
+        
+        #randomly sample the list of possible nucleotides and mutate the original sequence
+        subs = random.choice(possible_nucleotides)
+        mutated_sequence = sequence[:index] + subs + sequence[index + 1:]
+        sequence_alts.append((index, mutated_sequence))
+    
+    return sequence_alts 
+
+
+
+
+def in_silico_mutagenesis(tokenizer, inputs, labels, model, device_):  #make sure X_test is a list (use .flatten().tolist() when working with a numpy array)
+    r"""Pass mutated sequences through the model 
+    in evaluation mode
+    
+    Arguments:
+        use_tokenizer (:obj:`transformers.tokenization_?`):
+            Transformer type tokenizer used to process raw sequences into numbers.
+            
+        inputs (:obj:`str`):
+            input sequences.
+    
+       dataloader (:obj:`torch.utils.data.dataloader.DataLoader`):
+             Parsed data into batches of tensors.
+             
+       model (:obj:'our model')
+       device_ (:obj:`torch.device`):
+             Device used to load data before model training.
+    Returns:
+      
+      :obj: dictionary of mutation index as keys and predicted probabilities as values.
+    """
+
+    mutated_seqs = [] 
+    for index, seq in enumerate(inputs):
+        mutated_seqs.append(generate_mutated_sequences(seq))
+    ordered_seqs = list(map(list, zip(*mutated_seqs))) 
+    
+    all_predictions = []
+
+    for group_index, group in enumerate(ordered_seqs): 
+        sequences = [seq for _, seq in group]
+        
+    #    print(f"Group {group_index + 1} sequences: {sequences}") #line for debugging
+        
+        encodings = FivePrimeSeqs(sequences, tokenizer, labels)
+    
+     #   print(f"Encoding for Group {group_index + 1}: {encodings}") #line for debugging
+        
+        #start validation 
+    
+        probabilities = []
+        model.eval()
+    
+        batch = {k:v.type(torch.long).to(device_) for k,v in encodings.inputs.items()}
+    
+        with torch.no_grad():  
+            outputs = model(**batch) 
+            
+            loss, logits = outputs[:2] 
+              
+            # Move logits and labels to CPU
+            logits = logits.detach().cpu().numpy() 
+            
+            #get prediction probabilities by passing the model's logits through a Softmax function
+            probabilities += nn.functional.softmax(torch.tensor(logits), dim=-1) 
+            
+            #convert probabilities to a numpy array 
+            prediction_proba = torch.stack(probabilities) 
+            prediction_proba = prediction_proba.numpy()  
+            prediction_prob = prediction_proba[:, 1:2] 
+            
+            all_predictions.append((group_index, prediction_prob))
+            
+    all_predictions_dict = {index: predictions for index, predictions in all_predictions}     
+         
+ 
+    return all_predictions_dict 
+
+
+
+
+def predict_wild_type(tokenizer, inputs, labels, model, device_):
+    
+    r"""Get predictions for the wild type sequences
+    
+    Arguments:
+        use_tokenizer (:obj:`transformers.tokenization_?`):
+            Transformer type tokenizer used to process raw sequences into numbers.
+            
+        inputs (:obj:`str`):
+            input sequences.
+    
+       dataloader (:obj:`torch.utils.data.dataloader.DataLoader`):
+             Parsed data into batches of tensors.
+             
+       model (:obj:'our model')
+       device_ (:obj:`torch.device`):
+             Device used to load data before model training.
+    Returns:
+      
+      :obj: 'List of [wild type data, predicted probabilities, attention scores]
+    """
+    
+    wd_data = FivePrimeSeqs(inputs, tokenizer, labels)
+    
+    probabilities = []
+
+    model.eval()
+    
+    batch = {k:v.type(torch.long).to(device_) for k,v in wd_data.inputs.items()}
+    
+    with torch.no_grad():
+        outputs = model(**batch) 
+        
+        
+        attention = outputs.attentions[-1] 
+        loss, logits = outputs[:2] 
+          
+        logits = logits.detach().cpu().numpy() 
+        attentions = attention.detach().cpu().numpy() 
+        
+        #get prediction probabilities by passing the model's logits through a Softmax function
+        probabilities += nn.functional.softmax(torch.tensor(logits), dim=-1) 
+        
+        #convert probabilities to a numpy array 
+        prediction_proba = torch.stack(probabilities) 
+        prediction_proba = prediction_proba.numpy()  
+        prediction_prob = prediction_proba[:, 1:2]
+        
+        
+    return wd_data, prediction_prob, attentions 
+
+
+     
+def plot_ism_predictions(tokenizer, inputs, labels, model, device_):
+    r"""Plots predicted probabilities for mutated sequences against 
+    predicted probabilities for wild type.
+   
+   Arguments:
+       use_tokenizer (:obj:`transformers.tokenization_?`):
+           Transformer type tokenizer used to process raw sequences into numbers.
+           
+       inputs (:obj:`str`):
+           input sequences.
+   
+      dataloader (:obj:`torch.utils.data.dataloader.DataLoader`):
+            Parsed data into batches of tensors.
+            
+      model (:obj:'our model')
+      device_ (:obj:`torch.device`):
+            Device used to load data before model training.
+   Returns:
+     
+     :obj: 'List of [wild type encodings, wild type data, 
+     wild type attention scores, predicted probabilities for mutated sequences]
+   """
+   
+    mutate_preds = in_silico_mutagenesis(tokenizer, inputs, labels, model, device_)
+    
+    wd_encodings, wd_preds, wd_attentions = predict_wild_type(tokenizer, inputs, labels, model, device_) 
+ 
+    num_sequences = wd_preds.shape[0]
+
+    for sequence_index in range(num_sequences):
+        
+        plt.figure(figsize=(8, 5))
+    
+    # Wild type sequence
+        wild_type_probs = wd_preds[sequence_index]
+        plt.scatter([0], wild_type_probs, color='red')
+    
+    # Mutated sequences
+        mutated_indices = list(mutate_preds.keys())
+        mutated_probs = [mutate_preds[idx][sequence_index] for idx in mutated_indices]
+        plt.scatter(mutated_indices, mutated_probs, label='Mutated', color='black') 
+        plt.axhline(y=wild_type_probs, color='red', linestyle='--', label='Wild Type Prediction')
+
+    
+    # Add labels and a title
+        plt.xlabel('Mutation Index')
+        plt.ylabel('Prediction Probability')
+        plt.title(f'Sequence {sequence_index + 1} Predictions')
+        plt.ylim(0, 1)
+    
+    # Show the legend
+        plt.legend()
+    
+    # Show the plot
+        plt.show()
+        
+    return wd_encodings, wd_preds, wd_attentions, mutate_preds  
+
+    
+
+def calculate_gc_percentage(sequence):
+    r"""Calculates GC percentage of a sequence. 
+    Arguments:
+        sequence (:obj:`str`):
+            single input sequence.
+    Returns: 
+        GC Percentage of the sequence.
+    """
+    
+    gc_count = 0
+
+    for base in sequence:
+        if base.upper() in ['G', 'C']:
+            gc_count += 1
+
+    total_bases = len(sequence)
+    gc_percentage = (gc_count / total_bases) * 100
+
+    return gc_percentage 
+
+
